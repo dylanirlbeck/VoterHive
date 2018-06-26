3b70fc2cd11b94726a1064da5e95fd3df6a0db05
irlbeck2 <irlbeck2@illinois.edu>
2018-06-26 10:44:52 -0500

    Initial Commit

---
+++ MobileVoterOutreachApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileVoterOutreachApplication
+//
+//  Created by Dylan  Irlbeck on 6/26/18.
+//  Copyright © 2018 Dylan  Irlbeck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
