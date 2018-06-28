842baea7cdc55d8f4ac078b1cbb1a31ec90accd3
irlbeck2 <irlbeck2@illinois.edu>
2018-06-28 07:22:12 -0500

    Here is the second commit. Set up the page view controller, added some other UI elements.

---
+++ MobileVoterOutreachApplication/PageViewController.swift
@@ -0,0 +1,101 @@
+//
+//  PageViewController.swift
+//  MobileVoterOutreachApplication
+//
+//  Created by Dylan  Irlbeck on 6/26/18.
+//  Copyright © 2018 Dylan  Irlbeck. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newVc(viewController: "WelcomePage"), self.newVc(viewController: "InformationPageOne"), self.newVc(viewController: "InformationPageTwo")]
+    }()
+    
+    var pageControl = UIPageControl()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = self
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+        self.delegate = self
+        configurePageControl()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func configurePageControl() {
+        pageControl = UIPageControl(frame: CGRect(x: 0, y: UIScreen.main.bounds.maxY - 50, width: UIScreen.main.bounds.width, height: 50))
+        pageControl.numberOfPages = orderedViewControllers.count
+        pageControl.currentPage = 0
+        pageControl.tintColor = UIColor.black
+        pageControl.pageIndicatorTintColor = UIColor.gray
+        pageControl.currentPageIndicatorTintColor = UIColor.white
+        self.view.addSubview(pageControl)
+        
+    }
+    func newVc(viewController: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: viewController)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        return orderedViewControllers[previousIndex]
+        }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard orderedViewControllers.count != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > nextIndex else {
+            return nil
+        }
+        return orderedViewControllers[nextIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = orderedViewControllers.index(of: pageContentViewController)!
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
