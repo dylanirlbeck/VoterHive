3595bc75defcfc29aadc9d357eafdf1f481eda3f
AK Alilonu <aalilonu@wigglytuff.cs.uchicago.edu>
2018-07-03 17:21:03 -0500

    Added code to send text messages to Bees

--- SendMessage/SendMessage/ViewController.swift
+++ SendMessage/SendMessage/ViewController.swift
@@ -8,44 +8,37 @@
 
 import UIKit
 import MessageUI
+import Messages
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func sendMessage(_ sender: Any) {
+        print("Hi!")
+        if !MFMessageComposeViewController.canSendText() {
+            print("SMS services are not available")
+        }
+        
+        print("Hello!")
+        let messageVC = MFMessageComposeViewController()
+        
+        messageVC.body = "Hello!";
+        messageVC.recipients = ["6028104885"]
+        messageVC.messageComposeDelegate = self;
+        
+        self.present(messageVC, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func sendMessage(_ sender: Any) {
-        
-        let messageVC = MFMessageComposeViewController()
-            
-            messageVC.body = "Hello!";
-            messageVC.recipients = ["6028104885"]
-        messageVC.messageComposeDelegate = self as? MFMessageComposeViewControllerDelegate;
-            
-            self.present(messageVC, animated: false, completion: nil)
-        }
-        
-        func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
-            switch (result.rawValue) {
-            case MessageComposeResult.cancelled.rawValue:
-                print("Message was cancelled")
-                self.dismiss(animated: true, completion: nil)
-            case MessageComposeResult.failed.rawValue:
-                print("Message failed")
-                self.dismiss(animated: true, completion: nil)
-            case MessageComposeResult.sent.rawValue:
-                print("Message was sent")
-                self.dismiss(animated: true, completion: nil)
-            default:
-                break;
-            }
-        }
 }
-
